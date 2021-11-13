8252c1295d061faad09b07076b8d7b9a39b52ca6
Joe Gasparich <joe.gasparich@gmail.com>
2021-11-13 17:09:11 +1300

    favicon and page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Reality Check`,
+    description: `Annie Gasparich MDS Capstone project.`,
+    author: `@joegasparich`,
+    siteUrl: `https://annierose.nz/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `reality-check`,
+        short_name: `rc`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React, { MouseEventHandler, useLayoutEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+
 import { useInView } from 'react-intersection-observer';
 import { StaticImage } from "gatsby-plugin-image";
 
 import Slider from "../components/Slider";
 
-import "@fontsource/montserrat";
 import "./index.css";
 
 function useWindowSize() {
@@ -49,6 +50,13 @@ const IndexPage = () => {
 
   return (
     <div className="page">
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Reality Check</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
+      </Helmet>
       <div className="logo" onClick={() => {aboutRef.current?.scrollIntoView({ behavior: "smooth" })}}>
         <StaticImage
           src="../images/logo.png"
